1bc840f6ba355f7412d9a1724bd072e55a543e10
aishkl <aishkl123@gmail.com>
2018-10-20 11:24:31 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,7 +1,8 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
-
+import numpy as np
 data = pd.read_csv('data/house_prices_multivariate.csv')
 
 from sklearn.feature_selection import SelectPercentile
@@ -9,4 +10,16 @@ from sklearn.feature_selection import f_regression
 
 
 # Write your solution here:
+def percentile_k_features(df, k=20):
+    X=df.iloc[:,0:-1]
+    y=df.iloc[:,-1]
+    
+    fs=SelectPercentile(f_regression,k)
+    X_new=fs.fit_transform(X,y)    
+    
+    sd=list(X.columns.values[np.argsort(fs.scores_)[:-8:-1]])
+    return sd
+
+percentile_k_features(data,k=20)
+
 
--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -8,4 +9,25 @@ from sklearn.ensemble import RandomForestClassifier
 
 
 # Your solution code here
+def rf_rfe(data):
+    X=data.iloc[:,:-1]
+    y=data.iloc[:,-1]
+    ls=[]
+    rf=RandomForestClassifier()
+    rfe = RFE(rf, X.shape[1]/2)
+    X_new=rfe.fit(X,y)
+    mask = rfe.support_ #list of booleans
+    new_features = [] # The list of your K best features
+    for bool, feature in zip(mask,X.columns.values):
+        if bool:
+            new_features.append(feature)
+    return new_features
+   
+        
+    
+
+rf_rfe(data)
+    
+
+
 
